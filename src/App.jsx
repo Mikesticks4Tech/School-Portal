8e8b087e2bfa5372bd379717f30203a7a001a1da
Michael Idowu <idowumichael900@gmail.com>
2026-05-10 17:22:45 -0400

    fixed frontend deployment

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Router basename="/school-portal">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace "School-Portal" with your repo name
 export default defineConfig({
   plugins: [react()],
-  base: "/school-portal/",
 });
